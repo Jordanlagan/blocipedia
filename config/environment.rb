c96d2af6364070d34a3cf6080d9ac60c7abb4105
Jordan Lagan <jordanlagan000@gmail.com>
2017-10-20 11:19:09 -0400

    Added welcome controller, implemented authentication with devise

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,15 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+   :tls => true,
+   :address => "smtp.gmail.com",
+   :port => "587",
+   :domain => "gmail.com",
+   :authentication => :login,
+   :user_name => "[username]",
+   :password => "[password]"
+ }
+
+
 # Load the Rails application.
 require_relative 'application'
 
