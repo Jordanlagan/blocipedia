25e6dd935189e03818fc8833aa038a1454cc16d4
Jordan Lagan <jordanlagan000@gmail.com>
2017-10-21 09:43:07 -0400

    added basic crud functionality to app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!( id: 1, email: "test@fakexx9398402fas.com", password: "helloworld" )
+
+5.times do |t|
+  Wiki.create!( id: t, title: "Sample Wiki #{t+1}", body: "sample text", private: false, user: user )
+end
+
+puts "Wikis seeded"
