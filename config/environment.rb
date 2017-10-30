5bc7dc5e3d822676ddab7a2d2676fb7f4d50d8c5
Jordan Lagan <jordanlagan000@gmail.com>
2017-10-30 17:02:06 -0400

    working on mailer

--- config/environment.rb
+++ config/environment.rb
@@ -2,13 +2,14 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
    :tls => true,
    :address => "smtp.gmail.com",
-   :port => "587",
+   :port => 587,
    :domain => "gmail.com",
-   :authentication => :login,
-   :user_name => "[username]",
-   :password => "[password]"
+   :authentication => "plain",
+   :user_name => "username",
+   :password => "pass"
  }
 
+ActionMailer::Base.perform_deliveries = true
 
 # Load the Rails application.
 require_relative 'application'
